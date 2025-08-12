264e0f1943de238cdbe404d699bb2811d59dfa56
amir4976 <amirhosain4976@gmail.com>
2025-08-12 10:26:24 +0330

    send factor campleted

--- src/components/module/Bills/SendFactor/SendFactor.tsx
+++ src/components/module/Bills/SendFactor/SendFactor.tsx
@@ -1,4 +1,4 @@
-import { ArrowDown2, SearchNormal } from "iconsax-reactjs";
+import { ArrowDown2, DollarCircle, SearchNormal, Send, Send2 } from "iconsax-reactjs";
 import React from "react";
 import Dropdown from "../../DropDown/DropDown";
 
@@ -10,21 +10,44 @@ function SendFactor() {
         <p className="text-gray-600 text-sm">آنها در پرواز او موفق می شوند</p>
       </div>
       <div className="w-full bg-white p-4 flex flex-col gap-10">
-        <p>گیرنده را انتخاب کنید</p>
-        <div className="w-full h-10 bg-gray-200 overflow-hidden rounded-3xl relative px-3 text-gray-700">
-          <input
-            type="text"
-            className="w-10/12 h-full focus:outline-none"
-            placeholder="نام یوزر را انتخاب کنید"
-          />
-          <SearchNormal className="absolute left-2 top-2 " />
+        <div className="flex flex-col gap-2">
+          <p>گیرنده را انتخاب کنید</p>
+          <div className="w-full flex justify-between items-center h-14 bg-gray-100 overflow-hidden rounded-2xl relative px-3 text-gray-700">
+            <input
+              type="text"
+              className="w-10/12 h-full focus:outline-none"
+              placeholder="نام یوزر را انتخاب کنید"
+            />
+            <SearchNormal />
+          </div>
         </div>
-        {/* fix it by drop down */}
-        <p>گیرنده را انتخاب کنید</p>
-        <div className="w-full h-10 bg-gray-200 overflow-hidden rounded-3xl flex justify-between items-center relative px-3 text-gray-700">
-          <p> سرویس تعمیر و نگهدرای</p>
-          <ArrowDown2 />
+
+        <div className="flex flex-col gap-2">
+          {/* fix it by drop down */}
+          <p>گیرنده را انتخاب کنید</p>
+          <div className="w-full h-14 bg-blue-100 overflow-hidden rounded-2xl flex justify-between items-center relative px-3 text-gray-700">
+            <p> سرویس تعمیر و نگهدرای</p>
+            <ArrowDown2 />
+          </div>
         </div>
+
+        <div className="flex flex-col gap-2">
+          <p>برچسب</p>
+          <div className="w-full h-14 border border-gray-300  overflow-hidden rounded-2xl flex justify-between items-center relative px-3 text-gray-700">
+            <DollarCircle />
+            <input
+              type="number"
+              min={1}
+              className="w-10/12 h-full focus:outline-none"
+              placeholder="نام یوزر را انتخاب کنید"
+            />
+          </div>
+        </div>
+
+        <button className="w-full rounded-2xl bg-blue-500 flex justify-center items-center h-14  gap-2">
+          <Send  color="#fff" variant="Bold"/>
+          <p className="text-white text-md">ارسال فاکتور</p>
+        </button>
       </div>
     </div>
   );
